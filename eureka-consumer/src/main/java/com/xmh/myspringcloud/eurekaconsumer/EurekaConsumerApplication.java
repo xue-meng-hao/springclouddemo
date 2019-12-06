8fe96576076556deca2698a43c786e5f15edf29b
薛猛浩 <xmhzwy@163.com>
2019-12-06 14:00:28 +0800

    修改application.yml,HelloController.java

--- eureka-consumer/src/main/java/com/xmh/myspringcloud/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/xmh/myspringcloud/eurekaconsumer/EurekaConsumerApplication.java
@@ -2,9 +2,16 @@ package com.xmh.myspringcloud.eurekaconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EurekaConsumerApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
---
+++ eureka-consumer/src/main/java/com/xmh/myspringcloud/eurekaconsumer/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.xmh.myspringcloud.eurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/")
+    public String hello(@RequestParam String name) {
+        name += "!";
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-producer");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hello/?name=" + name;
+        return restTemplate.getForObject(url, String.class);
+    }
+}
